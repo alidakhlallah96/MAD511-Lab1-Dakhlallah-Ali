9b1aa99ad3d1ebc02cdfc50274a8c57cc94e46d8
alidakhlallah96 <w0687251@myscc.ca>
2026-09-22 21:48:10 -0400

    start of assignment 2

--- app/src/main/java/com/example/mad511_lab1_dakhlallah_ali/MainActivity.kt
+++ app/src/main/java/com/example/mad511_lab1_dakhlallah_ali/MainActivity.kt
@@ -15,7 +15,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-// Just for commit
+// Just for commit - for assignment 2
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
